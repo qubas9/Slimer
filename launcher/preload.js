62295fd7b286bfd3d30e018492ae24b9562656af
qubas9 <dovrtik@gmail.com>
2025-03-18 23:57:37 +0100

    launcher move

--- launcher/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a safe API to renderer process
-contextBridge.exposeInMainWorld('electron', {
-    checkForUpdates: () => ipcRenderer.invoke('check-for-updates')
-});
