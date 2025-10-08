7a9f27206210ea07700c4ab193ddf76175855b50
qubas9 <dovrtik@gmail.com>
2025-10-08 03:29:46 +0200

    feat: version in menu is automaticly up to date

--- feedback.js
+++ feedback.js
@@ -1,7 +1,7 @@
 const Feedback = {
   url: "https://docs.google.com/forms/d/e/1FAIpQLSf0uOhSqOoGlKBKwCfAeQKKaHGlcCr5zbJ7SKkjBGPYIHPHvg/viewform?usp=pp_url&entry.286032006=v0.0.1",
 
-  load() {
+  load(callback) {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "./package.json", true);
     xhr.responseType = "json";
@@ -9,11 +9,13 @@ const Feedback = {
     xhr.onload = () => {
       if (xhr.status === 200) {
         const version = xhr.response.version || "unknown";
+        this.version = version;
 
         // nahradíme jen poslední část URL (=verzi)
         this.url = this.url.replace(/(entry\.286032006=).+$/, `$1${encodeURIComponent(version)}`);
 
         console.log("Odkaz připraven:", this.url);
+        if (callback) callback();
       } else {
         console.error("Chyba při načítání package.json:", xhr.status);
       }
